bf4da05641a8d89795155068c3dde998f6a5ccc6
Khalickov Bulat <khalickovbulat@gmail.com>
2023-07-30 22:52:05 +0300

    add form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .form import EmailPostForm
 
 # Create your views here.
 def post_list(request):
@@ -34,3 +35,20 @@ def post_detail(request, year, month, day, post):
     return render(request, 
                   'blog/post/detail.html', 
                   {'post':post})
+
+def post_share(request, post_id):
+    # Извлечь пост по его id
+    post = get_object_or_404(Post, 
+                             status=Post.Status.PUBLISHED,
+                             id=post_id)
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отпраить электронное письмо
+    else:
+        form = EmailPostForm() 
+    return render(request, 'blog/post/share.html', {'post':post,
+                                                    'form':form})
