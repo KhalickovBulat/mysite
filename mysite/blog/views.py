1a49b2926a625d7b17aa2129602ea1b0dd29c812
Khalickov Bulat <khalickovbulat@gmail.com>
2023-07-31 10:14:25 +0300

    bag

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from . models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .form import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get('page')
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -41,14 +42,21 @@ def post_share(request, post_id):
     post = get_object_or_404(Post, 
                              status=Post.Status.PUBLISHED,
                              id=post_id)
+    sent = False
     if request.method == 'POST':
         # Форма была передана на обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Поля формы успешно прошли валидацию
             cd = form.cleaned_data
-            # ... отпраить электронное письмо
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommended you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'console.Email-Backend', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm() 
     return render(request, 'blog/post/share.html', {'post':post,
-                                                    'form':form})
+                                                    'form':form,
+                                                    'sent':sent})
